40f71389a452c2dcf4f735e1791f1117a8007ae4
Julia Thomas <juliamthomas03@gmail.com>
2024-02-09 15:24:18 -0700

    Created Pen and DrawView

--- DrawingApp/app/src/main/java/com/example/drawingapp/MainActivity.kt
+++ DrawingApp/app/src/main/java/com/example/drawingapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.drawingapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewTreeObserver
 
 class MainActivity : AppCompatActivity()
 {
@@ -9,5 +10,16 @@ class MainActivity : AppCompatActivity()
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // TODO: Move to DrawingFragment; just used for testing
+        val paint: DrawView = findViewById(R.id.draw_view)
+        paint.viewTreeObserver.addOnGlobalLayoutListener(object : ViewTreeObserver.OnGlobalLayoutListener {
+            override fun onGlobalLayout() {
+                paint.viewTreeObserver.removeOnGlobalLayoutListener(this)
+                val width = paint.measuredWidth
+                val height = paint.measuredHeight
+                paint.init(height, width)
+            }
+        })
     }
 }
---
+++ DrawingApp/app/src/main/java/com/example/drawingapp/Pen.kt
@@ -0,0 +1,5 @@
+package com.example.drawingapp
+
+import android.graphics.Path
+
+class Pen(var color: Int, var strokeWidth: Int, var path: Path)
