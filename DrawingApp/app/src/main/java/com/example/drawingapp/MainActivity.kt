7a11c47514858cb8c13054e37a8b450e823582fa
Brittney Morales <brittneymrls@gmail.com>
2024-02-11 01:44:48 -0700

    created transaction between fragments in the mainactivity

--- DrawingApp/app/src/main/java/com/example/drawingapp/MainActivity.kt
+++ DrawingApp/app/src/main/java/com/example/drawingapp/MainActivity.kt
@@ -3,9 +3,11 @@ package com.example.drawingapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.ViewTreeObserver
+import com.example.drawingapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity()
 {
+    private val binding: ActivityMainBinding by lazy {ActivityMainBinding.inflate(layoutInflater)}
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
@@ -19,7 +21,33 @@ class MainActivity : AppCompatActivity()
                 val width = paint.measuredWidth
                 val height = paint.measuredHeight
                 paint.init(height, width)
+
+        val fragSplash = SplashScreenFragment()
+        fragSplash.setListener(){
+            val fragHome = HomeScreenFragment()
+            fragHome.setListener {
+                val fragDraw = DrawingFragment()
+                fragDraw.setListener {
+                    supportFragmentManager.popBackStack()
+                }
+
+                val fTrans = supportFragmentManager.beginTransaction()
+                fTrans.replace(R.id.fragmentContainerView, fragDraw)
+                fTrans.addToBackStack(null)
+                fTrans.commit()
             }
-        })
+
+            val fTrans = supportFragmentManager.beginTransaction()
+            fTrans.replace(R.id.fragmentContainerView, fragHome)
+            //ftrans.addToBackStack(null) //does a back button to the splash screen, we don't need it on the home screen.
+            fTrans.commit()
+        }
+
+        val fTrans = supportFragmentManager.beginTransaction()
+        fTrans.replace(R.id.fragmentContainerView, fragSplash)
+        fTrans.commit()
+
+        setContentView(binding.root)
+
     }
 }
