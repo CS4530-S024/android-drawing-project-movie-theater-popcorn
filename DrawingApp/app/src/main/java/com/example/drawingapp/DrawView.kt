23cb73676f3d7e9ffb35914645e3bd6493b19053
Julia Thomas <juliamthomas03@gmail.com>
2024-02-16 14:11:29 -0700

    Brought touch events into drawingFragment and simplified code

--- DrawingApp/app/src/main/java/com/example/drawingapp/DrawView.kt
+++ DrawingApp/app/src/main/java/com/example/drawingapp/DrawView.kt
@@ -4,25 +4,13 @@ import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Paint
-import android.graphics.Path
 import android.util.AttributeSet
 import android.util.Log
-import android.view.MotionEvent
 import android.view.View
-import kotlin.math.abs
 
 class DrawView(context: Context, attrs: AttributeSet) : View(context, attrs) {
-    private val touchTolerance = 4f // Used for line smoothing;
-    private var mX = 0f // Finger X position
-    private var mY = 0f // Finger Y position
-    private lateinit var mPath: Path
     private var paint: Paint = Paint() // Holds color and style info
-//    private var paths = ArrayList<Pen>() // Stores all strokes done by user
-    private var currentPen = Pen(paint.color, paint.strokeWidth.toInt(), Path())
-//    private var currentColor = Color.BLACK
-//    private var strokeWidth = 20
     private var bitmap: Bitmap? = null
-//    private var mCanvas: Canvas? = null
 //    private val mBitmapPaint = Paint(Paint.DITHER_FLAG)
 
     init {
@@ -34,164 +22,18 @@ class DrawView(context: Context, attrs: AttributeSet) : View(context, attrs) {
 //        mPaint.strokeCap = Paint.Cap.ROUND
 //        mPaint.alpha = 0xff // 0xff=255, sets transparency of pen
     }
-
-    // This instantiates the bitmap and object
-//    fun init(height: Int, width: Int) {
-//        mBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
-//        mCanvas = Canvas(mBitmap!!)
-//    }
-
-//    fun setColor(color: Int) {
-//        currentColor = color
-//    }
-
-//    fun setStrokeWidth(width: Int) {
-//        strokeWidth = width
-//    }
-
-//    fun undo() {
-//        if (paths.isNotEmpty()) {
-//            paths.removeAt(paths.size - 1)
-//            invalidate()
-//        }
-//    }
-
-    // Returns the current bitmap
-//    fun save(): Bitmap? {
-//        return mBitmap
-//    }
-
     fun setPaint(paint: Paint) {
         this.paint = paint
-    }
-
-    fun getPaint(): Paint {
-        return paint
     }
 
     fun setBitmap(bitmap: Bitmap) {
         this.bitmap = bitmap
     }
 
-    fun setCurrentPen(currentPen: Pen) {
-        this.currentPen = currentPen
-    }
-
-    fun getCurrentPen(): Pen {
-        return currentPen
-    }
-
     // Actual drawing takes place here
     override fun onDraw(canvas: Canvas) {
+        Log.d("DrawView", "ON DRAW TO SCREEN")
+        super.onDraw(canvas)
         canvas.drawBitmap(bitmap!!, 0f, 0f, paint)
-    }
-
-//    // Creates new stroke (adds to list)
-//    private fun touchStart(x: Float, y: Float) {
-//        mPath = Path()
-//        val fp = Pen(currentColor, strokeWidth, mPath)
-//        paths.add(fp)
-//
-//        // Remove any curve from the path
-//        mPath.reset()
-//
-//        // Sets starting point of line being drawn
-//        mPath.moveTo(x, y)
-//
-//        mX = x
-//        mY = y
-//    }
-//
-//    // if the move of finger on the screen is greater than the tolerance then we call the quadTo()
-//    // method which smooths the turns we create, by calculating the mean position between the
-//    // previous position and current position
-//    private fun touchMove(x: Float, y: Float) {
-//        val dx = abs(x - mX)
-//        val dy = abs(y - mY)
-//        if (dx >= touchTolerance || dy >= touchTolerance) {
-//            mPath.quadTo(mX, mY, (x + mX) / 2, (y + mY) / 2)
-//            mX = x
-//            mY = y
-//        }
-//    }
-//
-//    private fun touchUp() {
-//        mPath.lineTo(mX, mY)
-//    }
-//
-    // Determines which touch method is called
-//    override fun onTouchEvent(event: MotionEvent): Boolean {
-//        val x = event.x
-//        val y = event.y
-//        when (event.action) {
-//            MotionEvent.ACTION_DOWN -> {
-//                context.touchStart(x, y)
-//                invalidate()
-//            }
-//            MotionEvent.ACTION_MOVE -> {
-//                touchMove(x, y)
-//                invalidate()
-//            }
-//            MotionEvent.ACTION_UP -> {
-//                touchUp()
-//                invalidate()
-//            }
-//        }
-//        return true
-//    }
-
-
-    // Creates new stroke (adds to list)
-    private fun touchStart(x: Float, y: Float) {
-        mPath = Path()
-        currentPen = Pen(paint.color, paint.strokeWidth.toInt(), mPath)
-
-        // Remove any curve from the path
-        mPath.reset()
-
-        // Sets starting point of line being drawn
-        mPath.moveTo(x, y)
-
-        mX = x
-        mY = y
-    }
-
-    // if the move of finger on the screen is greater than the tolerance then we call the quadTo()
-    // method which smooths the turns we create, by calculating the mean position between the
-    // previous position and current position
-    private fun touchMove(x: Float, y: Float) {
-        val dx = abs(x - mX)
-        val dy = abs(y - mY)
-        if (dx >= touchTolerance || dy >= touchTolerance) {
-            mPath.quadTo(mX, mY, (x + mX) / 2, (y + mY) / 2)
-            mX = x
-            mY = y
-        }
-    }
-
-    private fun touchUp() {
-        mPath.lineTo(mX, mY)
-    }
-
-    // Determines which touch method is called
-    override fun onTouchEvent(event: MotionEvent): Boolean {
-        Log.d("DrawView","touch")
-        val x = event.x
-        val y = event.y
-        when (event.action) {
-            MotionEvent.ACTION_DOWN -> {
-                touchStart(x, y)
-                invalidate()
-            }
-            MotionEvent.ACTION_MOVE -> {
-                touchMove(x, y)
-                invalidate()
-            }
-            MotionEvent.ACTION_UP -> {
-                touchUp()
-                invalidate()
-            }
-        }
-        return true
     }
 }
--- DrawingApp/app/src/main/java/com/example/drawingapp/DrawingFragment.kt
+++ DrawingApp/app/src/main/java/com/example/drawingapp/DrawingFragment.kt
@@ -1,6 +1,6 @@
 package com.example.drawingapp
 
-import android.graphics.Canvas
+import android.annotation.SuppressLint
 import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.Path
@@ -8,21 +8,27 @@ import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.graphics.toColor
 import androidx.fragment.app.activityViewModels
 import com.example.drawingapp.databinding.FragmentDrawingBinding
+import kotlin.math.abs
 
 class DrawingFragment : Fragment()
 {
     private lateinit var binding: FragmentDrawingBinding
     private var switchScreenCallback: () -> Unit = {}
     private var paint = Paint()
-    private lateinit var bitmapCanvas: Canvas
+    private val touchTolerance = 4f // Used for line smoothing;
+    private var mX = 0f // Finger X position
+    private var mY = 0f // Finger Y position
+    private lateinit var currentPath: Path
     private val viewModel : DrawingViewModel by activityViewModels()
+    private lateinit var currPen: Pen
 
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -31,26 +37,18 @@ class DrawingFragment : Fragment()
         binding = FragmentDrawingBinding.inflate(layoutInflater)
 
         viewModel.setCurrentPen(Pen(Color.BLACK, 20, Path()))
-        binding.drawView.setCurrentPen(viewModel.currentPen.value!!)
-        viewModel.setColor(Color.BLACK.toColor())
-        viewModel.setPenWidth(20)
-        bitmapCanvas = Canvas(viewModel.bitmap.value!!)
-        bitmapCanvas.drawColor(Color.WHITE)
 
-        viewModel.color.observe(viewLifecycleOwner){
-            val tempPaint = binding.drawView.getPaint()
-            tempPaint.color = viewModel.color.value!!.toArgb()
-            binding.drawView.setPaint(tempPaint)
-            drawPath(viewModel.currentPen.value!!)
-        }
+        viewModel.bitmapCanvas.value!!.drawColor(Color.GRAY)
 
-        viewModel.currentPen.observe(viewLifecycleOwner){
-            drawPath(it)
-        }
+        paint.strokeWidth = viewModel.currentPen.value!!.strokeWidth.toFloat()
+        paint.color = viewModel.currentPen.value!!.color
 
-        viewModel.bitmap.observe(viewLifecycleOwner){
-            bitmapCanvas = Canvas(viewModel.bitmap.value!!)
-        }
+        binding.drawView.setBitmap(viewModel.bitmap.value!!)
+        binding.drawView.setPaint(paint)
+
+//        viewModel.bitmap.observe(viewLifecycleOwner){
+//            bitmapCanvas = Canvas(viewModel.bitmap.value!!)
+//        }
 
         //Goes back to the main screen
         binding.backButton.setOnClickListener{
@@ -62,10 +60,26 @@ class DrawingFragment : Fragment()
                 childFragmentManager, ColorSelectDialogFragment.TAG)
         }
 
-        binding.drawView.setOnClickListener {
-            Log.d("Drawing Fragment", "setOnClickListener test")
-            viewModel.setCurrentPen(binding.drawView.getCurrentPen())
-            drawPath(viewModel.currentPen.value!!)
+        binding.drawView.setOnTouchListener { v, event ->
+            when (event?.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    Log.d("touch", "TOUCH DOWN")
+                    touchStart(event.x, event.y)
+                }
+                MotionEvent.ACTION_MOVE -> {
+                    Log.d("touch", "TOUCH MOVE")
+                    touchMove(event.x, event.y)
+                }
+                MotionEvent.ACTION_UP -> {
+                    Log.d("touch", "TOUCH UP")
+                    touchUp()
+                }
+            }
+            currPen = Pen(paint.color, paint.strokeWidth.toInt(), currentPath)
+            viewModel.setCurrentPen(currPen)
+            drawPath()
+
+            v?.onTouchEvent(event) ?: true
         }
 
         // Inflate the layout for this fragment
@@ -73,15 +87,46 @@ class DrawingFragment : Fragment()
     }
 
 
+    // Creates new stroke (adds to list)
+    private fun touchStart(x: Float, y: Float) {
+        currentPath = Path()
+
+        // Sets starting point of line being drawn
+        currentPath.moveTo(x, y)
+
+        mX = x
+        mY = y
+
+    }
+
+    // if the move of finger on the screen is greater than the tolerance then we call the quadTo()
+    // method which smooths the turns we create, by calculating the mean position between the
+    // previous position and current position
+    private fun touchMove(x: Float, y: Float) {
+        val dx = abs(x - mX)
+        val dy = abs(y - mY)
+        if (dx >= touchTolerance || dy >= touchTolerance) {
+            currentPath.quadTo(mX, mY, (x + mX) / 2, (y + mY) / 2)
+            mX = x
+            mY = y
+        }
+    }
+
+    private fun touchUp() {
+        currentPath.lineTo(mX, mY)
+    }
+
+
     fun setListener(listener: () -> Unit){
         switchScreenCallback = listener
     }
 
-    private fun drawPath(currentPen: Pen) {
-        Log.d("DrawingFragment", "in drawPath")
-        paint.strokeWidth = currentPen.strokeWidth.toFloat()
-        bitmapCanvas.drawPath(currentPen.path, paint)
+    private fun drawPath() {
+        Log.d("DrawingFragment", "DRAWING PATH")
+        val currentPen = viewModel.currentPen.value!!
+        //viewModel.bitmapCanvas.value!!.drawPath(currentPen.path, paint)
         binding.drawView.setBitmap(viewModel.bitmap.value!!)
+        binding.drawView.setPaint(paint)
         binding.drawView.invalidate()
     }
 }
