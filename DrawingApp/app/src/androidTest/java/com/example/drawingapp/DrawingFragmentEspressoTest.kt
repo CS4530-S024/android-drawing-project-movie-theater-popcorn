2a34ac444cddf25f7da5df32081ba1781b2af289
Brittney Morales <brittneymrls@gmail.com>
2024-03-23 15:00:57 -0600

    updated tests for existing drawing and commented code
    implemented test to checkExistingDrawingNameExist() button,
    newDrawingSavedSuccessfully(),
    penColorUpdatesWithSliders()
    added new test to check for new and previous color of pen
    
    made helper methods to save a drawing, open an existing drawing, and to check the background color of a button

--- DrawingApp/app/src/androidTest/java/com/example/drawingapp/DrawingFragmentEspressoTest.kt
+++ DrawingApp/app/src/androidTest/java/com/example/drawingapp/DrawingFragmentEspressoTest.kt
@@ -1,11 +1,25 @@
 package com.example.drawingapp
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.graphics.drawable.Drawable
 import android.view.View
+import android.widget.Button
 import android.widget.SeekBar
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.blue
+import androidx.core.graphics.green
+import androidx.core.graphics.red
+import androidx.core.graphics.toColor
+import androidx.test.espresso.Espresso.onData
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.UiController
 import androidx.test.espresso.ViewAction
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.BoundedMatcher
 import androidx.test.espresso.matcher.ViewMatchers
@@ -16,17 +30,19 @@ import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import com.google.android.material.slider.Slider
+import junit.framework.TestCase.assertEquals
 import org.hamcrest.Description
 import org.hamcrest.Matcher
 import org.junit.Rule
 import org.junit.Test
+import org.junit.internal.Checks
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 @LargeTest
 class DrawingFragmentEspressoTest {
-    @get: Rule
-    val activityRule = ActivityScenarioRule(MainActivity::class.java)
+    @get: Rule val activityRule = ActivityScenarioRule(MainActivity::class.java)
+    @get:Rule val composeTestRule = createComposeRule()
 
     /***
      * component id holders
@@ -50,6 +66,8 @@ class DrawingFragmentEspressoTest {
     //Test: check the sliders (RGB) for the pen color exist
     //Test: check label exists for each color (RGB) slider exist
     //Test: check number value label exists for each color (RGB) slider exist
+    //Test: check the previous color button exists on the color dialog
+    //Test: check the new color button exists on the color dialog
     //Test: check the size slider for the pen exists
 
     @Test
@@ -69,9 +87,8 @@ class DrawingFragmentEspressoTest {
     }
     @Test
     fun checkExistingDrawingNameExist() {
-        //TODO: add steps for click on an existing drawing
-        //onView(withId(R.id.imageName)).check(matches(isDisplayed()))
-        TODO("not yet implemented")
+        onView(withText("Pick Existing Drawing")).check(matches(isDisplayed()))
+        componentExistCheck(R.id.existingDrawing)
     }
     @Test
     fun checkEraserButtonExist() {
@@ -128,6 +145,15 @@ class DrawingFragmentEspressoTest {
         createNewDrawing()
         componentExistCheck(R.id.penSizeSlider)
     }
+    fun checkPrevColorButtonExist() {
+        createNewDrawing()
+        clickButton(R.id.colorSelectButton)
+        componentExistCheck(R.id.prevPenColor)
+    }
+    fun checkNewColorButtonExist() {
+        createNewDrawing()
+        clickButton(R.id.colorSelectButton)
+        componentExistCheck(R.id.newPenColor)    }
     @Test
     fun checkSizeLabelSlidersExist() {
         createNewDrawing()
@@ -142,13 +168,14 @@ class DrawingFragmentEspressoTest {
     //TODO: Create test: check pen size updates when using slider
     //TODO: Create test: check pen color updates when using sliders
     //TODO: Create test: check eraser updates the canvas
-    @Test
-    fun drawingNameUpdatesWithSave() {
-        createNewDrawing()
-        //TODO: have existing drawings
-    TODO("not yet implemented")
 
-}
+    @Test
+    fun newDrawingSavedSuccessfully() {
+        createNewDrawing()
+        saveDrawing("test_save_first_drawing")
+
+
+    }
     @Test
     fun backButtonNavigatesToHomeScreen() {
         createNewDrawing()
@@ -160,7 +187,7 @@ class DrawingFragmentEspressoTest {
     fun penShapeUpdates() {
         createNewDrawing()
         clickButton(R.id.penButton)
-//        TODO("not yet implemented: needs options to click on")
+//      TODO("not yet implemented: needs options to click on")
 
     }
     private fun checkPeekBar(id: Int, expectedValue: Int){
@@ -169,6 +196,7 @@ class DrawingFragmentEspressoTest {
     private fun setPeekBar(id: Int, newValue: Int){
         onView((withId(id))).perform(setValue(newValue))
     }
+
 //    @Test
 //    fun penSizeUpdates() {
 //        createNewDrawing()
@@ -178,7 +206,7 @@ class DrawingFragmentEspressoTest {
 //        TODO check pen size
 //    }
     @Test
-    fun penColorUpdates() {
+    fun penColorUpdatesWithSliders() {
         var greenValue = 0
         var blueValue = 0
         var redValue = 255
@@ -189,16 +217,26 @@ class DrawingFragmentEspressoTest {
         checkPeekBar(blueBar, blueValue)
         checkPeekBar(greenBar, greenValue)
 
+        componentExistCheck(R.id.prevPenColor)
+        componentExistCheck(R.id.newPenColor)
+
+
+    //checks the penColor was set to Red for both the prevPenColor button and newPenColor button
+        onView(withId(R.id.prevPenColor)).check(matches(withValueColor(Color.rgb(redValue,greenValue,blueValue))))
+        onView(withId(R.id.newPenColor)).check(matches(withValueColor(Color.rgb(redValue,greenValue,blueValue))))
+
         greenValue = 255
         blueValue = 0
         setPeekBar(greenBar, greenValue)
         setPeekBar(blueBar,blueValue)
 
+        //Checking the sliders values were updated with the values given
         checkPeekBar(redBar,redValue)
         checkPeekBar(blueBar, blueValue)
         checkPeekBar(greenBar, greenValue)
 
-        //TODO: check Pen color
+        //checks the penColor was updated for the newPenColor button
+        onView(withId(R.id.newPenColor)).check(matches(withValueColor(Color.rgb(redValue,greenValue,blueValue))))
     }
     @Test
     fun eraserUpdatesCanvas() {
@@ -219,6 +257,23 @@ class DrawingFragmentEspressoTest {
         goToDrawFrag()
         //checks if Save text is on the current Screen to verify we are on the Drawing Fragment
         componentExistCheck(R.id.imageName)
+    }
+
+    private fun saveDrawing(fileName: String) {
+        componentExistCheck(R.id.imageName)
+        componentExistCheck(R.id.saveButton)
+        onView(withId(R.id.imageName)).perform(typeText(fileName))
+        onView(withId(R.id.imageName)).check(matches(withText(fileName)))
+        onView(withId(R.id.saveButton)).perform(click())
+    }
+
+    private fun openExistingDrawing(){
+        onView(withText("Pick Existing Drawing")).check(matches(isDisplayed()))
+        componentExistCheck(R.id.existingDrawing)
+        clickButton(R.id.existingDrawing)
+        composeTestRule.onNodeWithText("Go Back").assertExists()
+//        composeTestRule.onNodeWithText("Go Back").performClick()
+
     }
 
     private fun componentExistCheck(id: Int){
@@ -263,6 +318,21 @@ class DrawingFragmentEspressoTest {
             override fun perform(uiController: UiController?, view: View) {
                 val seekBar = view as SeekBar
                 seekBar.progress = value
+            }
+        }
+    }
+
+    // withValue Color was inspired from stackoverflow which allows espresso to check the button background color
+    // this is the link: https://stackoverflow.com/questions/74442653/create-a-viewmatcher-for-the-background-color-of-a-button
+    private fun withValueColor(expectedValue: Int): Matcher<View?> {
+        return object : BoundedMatcher<View?, Button>(Button::class.java) {
+            override fun describeTo(description: Description) {
+                description.appendText("expected: $expectedValue")
+            }
+
+            override fun matchesSafely(button: Button?): Boolean {
+                val backColor = (button?.background as ColorDrawable).color
+                return backColor == expectedValue
             }
         }
     }
