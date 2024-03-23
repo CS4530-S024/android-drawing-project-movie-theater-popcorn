5e1626b29fb27548cff243a9e22df8c50f658239
Daisy O <odaisy432@gmail.com>
2024-03-22 22:29:27 -0600

    Composable view can now display file name and go to drawing page

--- DrawingApp/app/src/main/java/com/example/drawingapp/DrawingListView.kt
+++ DrawingApp/app/src/main/java/com/example/drawingapp/DrawingListView.kt
@@ -1,6 +1,6 @@
 package com.example.drawingapp
 
-import android.graphics.*
+import android.graphics.Bitmap
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -14,9 +14,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.*
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.res.*
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.fragment.findNavController
@@ -34,80 +33,57 @@ class DrawingListView : Fragment() {
 
         //ComposeView gives us a `Composable` context to run functions in
         binding.composeView.setContent {
-            DrawingComposable(Modifier.padding(16.dp)){
-                findNavController().navigate(R.id.action_go_back_to_home_screen)
+            DrawingComposable(Modifier.padding(16.dp),
+                onClick = { findNavController().navigate(R.id.action_go_back_to_home_screen) }) {
+                    findNavController().navigate(R.id.action_open_saved_drawing)
             }
         }
 
         return binding.root
     }
 
-    
+
     @Composable
     fun DrawingComposable(modifier: Modifier = Modifier,
                           viewModel: DrawingViewModel = viewModel(
                               viewModelStoreOwner = LocalContext.current.findActivity()
                           ),
-                          onClick: ()->Unit)
+                          onClick: ()->Unit, onClickFile: () -> Unit)
     {
-        Column(modifier = modifier.padding(32.dp)) {
-            val currentDrawings by viewModel.allDrawings.collectAsState(listOf())
-//            ExistingDrawings(drawings = currentDrawings, modifier)
-//            Button(onClick = { /*TODO*/ }) {
-//
-//            }
-        }
+
         val currentDrawings by viewModel.allDrawings.collectAsState(listOf())
        // val bitmap = viewModel.loadDrawing(drawing)
         Box(modifier){
             Button(onClick = onClick) {
             Text("Go Back")
         }
-//            LazyColumn(Modifier.fillMaxWidth()) {
-//                items(currentDrawings){ drawing ->
-//                    viewModel.loadDrawing(drawing.filePath)
-//                    ExistingDrawingItem(
-//                        drawing = drawing,
-//                        bitmap = viewModel.bitmap.value
-//                    )
-//                }
-//            }
+            LazyColumn(Modifier.fillMaxWidth()) {
+                items(currentDrawings){ drawing ->
+ //                   viewModel.loadDrawing(drawing.filePath)
+                   ExistingDrawingItem(
+                       drawing = drawing,
+                       bitmap = viewModel.bitmap.value,
+                       onClick = onClickFile
+                    )
+                }
+            }
         }
     }
 
     @Composable
-    fun ExistingDrawingItem(drawing: DrawingData, modifier: Modifier = Modifier, bitmap: Bitmap?)
+    fun ExistingDrawingItem(drawing: DrawingData, modifier: Modifier = Modifier, bitmap: Bitmap?, onClick: () -> Unit)
     {
-        Column {
+        Row {
 
             if (bitmap != null)
             {
-                Image(bitmap = bitmap.asImageBitmap(), contentDescription = "")
+                Image(painter = painterResource(id = R.drawable.ic_color_palete), contentDescription = "")
             }
-            Text(text = drawing.fileName)
+            Button(onClick = onClick) {
+                Text(text = drawing.fileName)
+
+            }
 
         }
-    }
-
-//    @Composable
-//    fun ExistingDrawings(drawings: List<DrawingData>, modifier: Modifier = Modifier)
-//    {
-//        Box(modifier){
-//            LazyColumn(Modifier.fillMaxWidth()) {
-//                items(drawings){ drawing ->
-//                    ExistingDrawingItem(
-//                        drawing = drawing,
-//                        bitmap = viewModel.loadDrawing(drawing)
-//
-//                    )
-//                }
-//            }
-//        }
-//    }
-
-    @Preview(showBackground = true)
-    @Composable
-    fun test(){
-        //ExistingDrawingItem(drawing = "test", bitmap =)
     }
 }
